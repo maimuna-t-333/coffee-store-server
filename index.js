15e300dace43aa9bacd1670a79849f227f6b3dd5
maimuna-t-333 <maimuna.tabr@gmail.com>
2025-05-14 21:12:14 +0600

    coffee server

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app=express();
+const port=process.env.PORT || 3000;
+
+
+app.use(cors())
+app.use(express.json())
+
+// console.log(process.env.DB_USER)
+// console.log(process.env.DB_PASS)
+
+
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@xenpi.9qo9gbb.mongodb.net/?retryWrites=true&w=majority&appName=XenPi`;
+// const uri = "mongodb+srv://<db_username>:<db_password>@xenpi.9qo9gbb.mongodb.net/?retryWrites=true&w=majority&appName=XenPi";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const coffeeCollection=client.db('coffeeDB').collection('coffees')
+
+    app.post('/coffees',async(req,res)=>{
+        const newCoffee=req.body;
+        console.log(newCoffee)
+        const result=await coffeeCollection.insertOne(newCoffee)
+        res.send(result)
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/',(req,res)=>{
+    res.send('coffee server is getting hotter')
+})
+
+app.listen(port,()=>{
+    console.log(`coffee server is running on port ${port}`)
+})
